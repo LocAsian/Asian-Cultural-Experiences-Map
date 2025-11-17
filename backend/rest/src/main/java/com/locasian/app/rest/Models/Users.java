35e75e2260d6055b5cfa78c48ad6c5597429f27c
Jake Fulton <146018017+jakefulton1@users.noreply.github.com>
2025-11-16 23:41:31 -0500

    finished Users backend/database

--- backend/rest/src/main/java/com/locasian/app/rest/Models/Users.java
+++ backend/rest/src/main/java/com/locasian/app/rest/Models/Users.java
@@ -1,39 +1,39 @@
 package com.locasian.app.rest.Models;
 import java.time.LocalDateTime;
-import javax.persistence.*;
+import jakarta.persistence.*;
 
 @Entity
 public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int user_id;
+    private long user_id;
     @Column
     private String username;
     @Column
     private LocalDateTime created_at;
 
 
-public int get_user_id() {
+public long getUser_id() {
     return user_id;
 }
 
-public void set_user_id(int user_id) {
+public void setUser_id(long user_id) {
     this.user_id = user_id;
 }
 
-public String get_username() {
+public String getUsername() {
     return username;
 }
 
-public void set_username(String username) {
+public void setUsername(String username) {
     this.username = username;
 }
 
-public LocalDateTime get_created_at() {
+public LocalDateTime getCreated_at() {
     return created_at;
 }
 
-public void set_created_at(LocalDateTime created_at) {
+public void setCreated_at(LocalDateTime created_at) {
     this.created_at = created_at;
 } 
 }
--- backend/rest/src/main/java/com/locasian/app/rest/Repo/UsersRepo.java
+++ backend/rest/src/main/java/com/locasian/app/rest/Repo/UsersRepo.java
@@ -3,6 +3,6 @@ package com.locasian.app.rest.Repo;
 import com.locasian.app.rest.Models.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepo() extends JpaRepository<User, int> {
+public interface UsersRepo extends JpaRepository<Users, Long> {
 
 }
